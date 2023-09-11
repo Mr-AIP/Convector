06e9fc2cde2bd5c75606831baf6dbc76fb20679b
Mr-AIP <100208208+Mr-AIP@users.noreply.github.com>
2023-09-11 15:27:06 +0300

    Add files via upload

---
+++ ConverterApp/PhysicValuesLib/Values/Weight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicValuesLib.Values
+{
+    internal class Weight: AnyValue
+    {
+        public Weight()
+        {
+            ValueName = "Масса";
+
+            CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+            { "Грамм",        0.001       },
+            {"Милигармм", 0.000001},
+            {"Тонна", 1000},
+            {"Центнер", 100},
+            {"Килограмм", 1 },
+           
+        };
+        }
+    }
+}
---
+++ ConverterApp/WindowsAPI/ConverterForm.cs
@@ -0,0 +1,63 @@
+using PhysicValuesLib;
+
+namespace WindowsAPI
+{
+    public partial class ConverterForm : Form
+    {
+        private ConverterManager _manager;
+
+        public ConverterForm()
+        {
+            InitializeComponent();
+            _manager = new ConverterManager();
+        }
+
+        private void ConverterFormLoad(object sender, EventArgs e)
+        {
+            ListOfValues.Items.AddRange(_manager.GetPhysicValuesList().ToArray());
+            ListOfValues.SelectedIndex = 0;          
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TransferFrom.Items.Clear();
+            TransferTo.Items.Clear();
+            TransferFrom.Items.AddRange(_manager.GetMeasureList(ListOfValues.SelectedItem.ToString()).ToArray());
+            TransferTo.Items.AddRange(_manager.GetMeasureList(ListOfValues.SelectedItem.ToString()).ToArray());
+            TransferFrom.SelectedIndex = 0;
+            TransferTo.SelectedIndex = 0;
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double value = Convert.ToDouble(textBox1.Text);         // считали число
+                string physicValue = ListOfValues.SelectedItem.ToString();
+                string from = TransferFrom.SelectedItem.ToString();
+                string to = TransferTo.SelectedItem.ToString();
+                double result = _manager.GetConvertedValue(physicValue, value, from, to);
+                textBox2.Text = result.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TransferFrom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TranfersTo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
